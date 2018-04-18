d46857dc15a671cbbc0c6d8cf7ad0c22a8abe4c9
Ibeau <iibeau@LAMU02W65RVHTD6.uhc.com>
2018-04-18 11:14:07 +0100

    Added Version and updated test

---
+++ src/main/java/com/optum/testdir/Constants.java
@@ -0,0 +1,6 @@
+package com.optum.testdir;
+
+public class Constants {
+
+    public static final String VERSION = "v1";
+}
--- src/test/groovy/com/optum/testdir/ClinicalIndicators.groovy
+++ src/test/groovy/com/optum/testdir/ClinicalIndicators.groovy
@@ -1,5 +1,7 @@
 package com.optum.testdir
 
+import com.jayway.restassured.config.ConnectionConfig
+import com.jayway.restassured.path.json.config.JsonPathConfig
 import com.jayway.restassured.response.Response
 import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Value
@@ -10,13 +12,16 @@ import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.web.WebAppConfiguration
 import spock.lang.Specification
 
+import java.util.concurrent.TimeUnit
+
 import static com.jayway.restassured.RestAssured.given
 import static com.jayway.restassured.config.LogConfig.logConfig
 import static com.jayway.restassured.config.RestAssuredConfig.newConfig
+import static com.optum.testdir.Constants.VERSION
 
 @WebAppConfiguration
 @ContextConfiguration(classes = IntegrationTestDirApplication, loader = SpringApplicationContextLoader)
-@TestPropertySource(locations="classpath:test.properties")
+@TestPropertySource(locations = "classpath:test.properties")
 public class ClinicalIndicators extends Specification {
 
     @Value("\${baseUrl}")
@@ -26,26 +31,37 @@ public class ClinicalIndicators extends Specification {
     private int port;
 
 
-@CompileStatic
-def dir() {
+    @CompileStatic
+    def dir() {
 
-    given()
-        .config(newConfig().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
-        .baseUri(baseURI)
-        .port(8080)
-}
+        given()
+            .config(newConfig().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails())
+            .connectionConfig(ConnectionConfig.connectionConfig()
+            .closeIdleConnectionsAfterEachResponseAfter(20, TimeUnit.SECONDS)))
+            .baseUri(baseURI)
+            .port(port)
+    }
 
 
-    def "get /dir should return a clinical indicator"(int id, int code) {
+    def "get /dir should return a clinical indicator"(int id, int statusCode, int serviceGroupId,
+                                                      String indicationId, String testId, String testName,
+                                                      String technology, String targetGene) {
 
         expect:
-        Response response = dir().get("/clinical-indicators/{id}", id)
-        response.statusCode() == code
+        Response response = dir().get("/" + VERSION + "/clinical-indicators/{id}", id)
+        response.statusCode() == statusCode
+        response.contentType().equals("application/json; charset=utf-8")
 
-         where:
-         id | code
-         1  | 404
-         2  | 404
+        Integer.valueOf(response.body().jsonPath().getString("specialist_service_group_id")) == serviceGroupId
+        response.body().jsonPath().getString("test_ID_code").equals(testId)
+        response.body().jsonPath().getString("test_name").equals(testName)
+        response.body().jsonPath().get("technology").equals(technology)
+        response.body().jsonPath().get("target_gene").equals(targetGene)
+        response.body().jsonPath().get("clinical_indication_ID_code").equals(indicationId)
+
+        where:
+        id   | statusCode | serviceGroupId | indicationId | testId   | testName                 | technology                       | targetGene
+        1001 | 200        | 93             | "C153"       | "C153.6" | "BRAF-AKAP9 FISH/RT-PCR" | "FISH/Targeted mutation testing" | "BRAF-AKAP9"
     }
 
 }
