f4e773f295a2a0cd689956426c08f0d1bb94c814
Madhu Donepudi <mdonepu1@LAMU02VQ137HTD6.uhc.com>
2018-04-20 09:40:36 +0100

    added tests for Genes endpoints

---
+++ src/test/groovy/com/optum/testdir/Genes.groovy
@@ -0,0 +1,93 @@
+package com.optum.testdir
+
+import com.jayway.restassured.config.ConnectionConfig
+import com.jayway.restassured.response.Response
+import groovy.transform.CompileStatic
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.web.WebAppConfiguration
+import spock.lang.Specification
+
+import java.util.concurrent.TimeUnit
+
+import static com.jayway.restassured.RestAssured.given
+import static com.jayway.restassured.config.LogConfig.logConfig
+import static com.jayway.restassured.config.RestAssuredConfig.newConfig
+
+@WebAppConfiguration
+@ContextConfiguration(classes = IntegrationTestDirApplication, loader = SpringApplicationContextLoader)
+@TestPropertySource(locations = "classpath:test.properties")
+public class Genes extends Specification {
+
+    @Value("\${baseUrl}")
+    private String baseURI;
+
+    @Value("\${server.port}")
+    private int port;
+
+    @Value("\${version}")
+    private String version
+
+
+    @CompileStatic
+    def dir() {
+
+
+        given()
+            .config(newConfig().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails())
+            .connectionConfig(ConnectionConfig.connectionConfig()
+            .closeIdleConnectionsAfterEachResponseAfter(20, TimeUnit.SECONDS)))
+            .baseUri(baseURI)
+            .port(port)
+
+    }
+
+    def "get /genes with limit query should limit the listing"(int limit, int statusCode) {
+
+
+        expect:
+        Response response = dir()
+            .queryParam("limit",limit)
+            .get("/" + version + "/genes")
+
+        response.statusCode() == statusCode
+        response.contentType().equals("application/json; charset=utf-8")
+        int resultsSize = response.body().jsonPath().getList("\$").size()
+        assert resultsSize <= limit
+
+        where:
+        limit | statusCode
+        4     | 200
+    }
+
+    def "get /genes should return list of target genes"() {
+
+        expect:
+        Response response = dir()
+            .get("/" + version + "/genes")
+        response.statusCode() == 200
+        response.contentType().equals("application/json; charset=utf-8")
+        assert response.body().jsonPath().getList("\$").size() >= 1
+
+    }
+
+    def "get /genes-autocomplete should return suggestions for target genes based on the characters typed"(String queryString, int statusCode) {
+        expect:
+        Response response = dir()
+            .queryParam("query", queryString)
+            .get("/" + version + "/genes-autocomplete")
+        response.statusCode() == statusCode
+        def resultList = response.body().jsonPath().getList("\$")
+        for (item in resultList) {
+            assert item.toString().toLowerCase().contains(queryString.toLowerCase())
+        }
+
+        where:
+        queryString               | statusCode
+        "BRCA"                    | 200
+        "panel"                   | 200
+        "inva"                    | 200 //invalid query should return no results
+    }
+}
